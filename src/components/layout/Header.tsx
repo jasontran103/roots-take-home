f55e49838ce2baf8e260039dec2e7e2ec17f8881
jasontran103 <jasontran103@gmail.com>
2025-05-14 14:54:30 -0700

    style: Added some padding to better fit the screen

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { HeartIcon, UserIcon, HomeIcon } from 'lucide-react';
 
 const Header = () => (
   <div className="bg-white border-b-4 border-neo-black py-4">
-    <div className="mx-auto flex justify-between items-center px-4">
+    <div className="mx-auto flex justify-between items-center px-4 ">
       <div className="flex items-center">
         <div className="w-10 h-10 bg-neo-primary border-4 border-neo-black flex items-center justify-center mr-3">
           <HomeIcon className="h-6 w-6 text-neo-black" />
